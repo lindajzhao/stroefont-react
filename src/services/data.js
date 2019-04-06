338905cd9dc19c79f0f74073e14f98c847028dd0
Linda <lindajzhao@gmail.com>
2019-04-06 18:01:12 -0400

    Fetch data from mock API

--- src/services/data.js
+++ src/services/data.js
@@ -1,6 +1,11 @@
+import axios from 'axios';
+// const axios = require('axios');
+
 const DataService = {
-    getData: function getData() {
+    getData: async function getData() {
         console.log('running DataService.getData');
+        let res = await axios.get('https://my.api.mockaroo.com/mock_drug_catalogue.json?key=73bb4560')
+        return res;
     }
 }
 
